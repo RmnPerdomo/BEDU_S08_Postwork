1a7a492fd7bbf2f3173dc3865cf415534ceb0472
dnsmartinez <dennis220981@gmail.com>
2022-12-11 22:54:31 -0600

    Cambio de variables categóricas y conversión a factor

--- Postwork_Sesion08_Dmi.R
+++ Postwork_Sesion08_Dmi.R
@@ -2,69 +2,90 @@
 # Postwork sesión 8. Análisis de la Inseguridad Alimentaria en México
 
 #### OBJETIVO
-
-#- Realizar un análisis estadístico completo de un caso 
-#- Publicar en un repositorio de Github el análisis y el código empleado 
+"
+- Realizar un análisis estadístico completo de un caso
+ - Publicar en un repositorio de Github el análisis y el código empleado 
+"
 
 #### REQUISITOS
-
-#- Haber realizado los works y postworks previos 
-#- Tener una cuenta en Github o en RStudioCloud
+"
+- Haber realizado los works y postworks previos 
+- Tener una cuenta en Github o en RStudioCloud
+"
 
 #### DESARROLLO
 
-"Un centro de salud nutricional está interesado en analizar estadísticamente
+"
+Un centro de salud nutricional está interesado en analizar estadísticamente
 y probabilísticamente los patrones de gasto en alimentos saludables y no
 saludables en los hogares mexicanos con base en su nivel socioeconómico,
 en si el hogar tiene recursos financieros extrar al ingreso y en si presenta
 o no inseguridad alimentaria. Además, está interesado en un modelo que le
 permita identificar los determinantes socioeconómicos de la inseguridad
-alimentaria."
+alimentaria.
+"
 
-"La base de datos es un extracto de la Encuesta Nacional de Salud y
+"
+La base de datos es un extracto de la Encuesta Nacional de Salud y
 Nutrición (2012) levantada por el Instituto Nacional de Salud Pública en México.
 La mayoría de las personas afirman que los hogares con menor nivel
 socioeconómico tienden a gastar más en productos no saludables que las personas
 con mayores niveles socioeconómicos y que esto, entre otros determinantes,
-lleva a que un hogar presente cierta inseguridad alimentaria."
+lleva a que un hogar presente cierta inseguridad alimentaria.
+"
 
+"
 La base de datos contiene las siguientes variables:
 - nse5f (Nivel socieconómico del hogar):
-    1 "Bajo", 2 "Medio bajo", 3 "Medio", 4 "Medio alto", 5 "Alto".
-- area (Zona geográfica): 0 "Zona urbana", 1 "Zona rural".
+    1 'Bajo', 2 'Medio bajo', 3 'Medio', 4 'Medio alto', 5 'Alto'.
+- area (Zona geográfica): 0 'Zona urbana', 1 'Zona rural'.
 - numpeho (Número de persona en el hogar)
-- refin (Recursos financieros distintos al ingreso laboral): 0 "no", 1 "sí"
+- refin (Recursos financieros distintos al ingreso laboral): 0 'no', 1 'sí'
 - edadjef (Edad del jefe/a de familia)
-- sexoje (Sexo del jefe/a de familia): 0 "Hombre", 1 "Mujer"
+- sexoje (Sexo del jefe/a de familia): 0 'Hombre', 1 'Mujer'
 - añosedu (Años de educación del jefe de familia)
 - ln_als (Logarítmo natural del gasto en alimentos saludables)
 - ln_alns (Logarítmo natural del gasto en alimentos no saludables)
-- IA (Inseguridad alimentaria en el hogar): 0 "No presenta IA", 1 "Presenta IA"
-
+- IA (Inseguridad alimentaria en el hogar): 0 'No presenta IA', 1 'Presenta IA'
+"
 
 df <- read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-R-Santander-2022/main/Sesion-08/Postwork/inseguridad_alimentaria_bedu.csv")
+df.fact <- df
 
+"
 1) Plantea el problema del caso
+"
 
 
-2) Realiza un análisis descriptivo de la información
+" 2) Realiza un análisis descriptivo de la información "
 
-# La estructura del df muestra que las variables son numéricas (int y num)
+# La estructura del df muestra que todas las variables son 'int' o 'num'
 str(df)
-# La función muestra datos faltantes en edadjef, sexojef, ln_als, ln_alns
+
+# La función muestra datos faltantes en 'edadjef', 'sexojef', 'ln_als', 'ln_alns'
 summary(df)
 
-# Cambio de las variables categóricas
-library(dplyr)
-df2 <- df %>%
-    mutate(nseh = factor(nse5f,
-                         labels = c("Bajo", "Medio bajo", "Medio", "Medio alto", "Alto")
-                         )
-           )
+# Cambio de valores y conversión a factor para la columna nse5f
+df.fact$nse5f[df.fact$nse5f == 1] <- "Bajo"
+df.fact$nse5f[df.fact$nse5f == 2] <- "Medio bajo"
+df.fact$nse5f[df.fact$nse5f == 3] <- "Medio"
+df.fact$nse5f[df.fact$nse5f == 4] <- "Medio alto"
+df.fact$nse5f[df.fact$nse5f == 5] <- "Alto"
+df.fact$nse5f <- factor(x = df.fact$nse5f,
+                        levels = c("Bajo", "Medio bajo", "Medio", "Medio alto",
+                                   "Alto"), ordered = TRUE)
+# Cambio a factor para la columna area
+df.fact$area <- factor(df.fact$area, labels = c("Zona urbana", "Zona rural"))
+# Cambio de valores para la columna efin
+df.fact$refin <- factor(df.fact$refin, labels = c("no", "si"))
+# Cambio de valores para la columna sexoje
+df.fact$sexojef <- factor(df.fact$sexojef, labels = c("Hombre", "Mujer"))
+# Cambio de valores para la columna IA
+df.fact$IA <- factor(df.fact$IA, labels = c("No presenta IA","Presenta IA"))
 
-summary(df)
-summary(df2)
-str(df2)
+summary(df.fact)
+
+
 
 3) Calcula probabilidades que nos permitan entender el problema en México
 
